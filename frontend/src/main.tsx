41d166d9d6b23c7b920551ec7d00e6b6631b98ee
Junaid Atari <mj.atari@gmail.com>
2026-04-30 19:48:17 +0500

    feat: add WebSocket context provider for real-time communication
    Add WebSocketProvider to wrap the App component, enabling real-time communication
    features. This change introduces a new context provider for managing WebSocket
    connections, which will be used to implement live updates and interactive
    functionality.
    
    No breaking changes, as the existing ThemeProvider remains unchanged and
    WebSocketProvider is added as a new layer in the component hierarchy.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { ThemeProvider } from './context/ThemeContext'
+/**
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @copyright 2026 Junaid Atari
+ * @see https://github.com/blacksmoke26
+ */
+
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+import {ThemeProvider} from './context/ThemeContext';
+import {WebSocketProvider} from './context/WebSocketContext';
+
+// styles
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <WebSocketProvider>
+        <App/>
+      </WebSocketProvider>
     </ThemeProvider>
   </StrictMode>,
-)
+);
